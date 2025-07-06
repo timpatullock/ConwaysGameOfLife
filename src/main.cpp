c880abe008637f1b37b7c3b16d42f2c8d22116a0
Tim Patullock <oddalicious@gmail.com>
2025-07-06 16:32:25 +1000

    Upadte TerminalRenderer.h

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main()
     constexpr int cellSize = 10;
 
     std::unique_ptr<Renderer> renderer = std::make_unique<RaylibRenderer>(cellSize, cols, rows);
-    // Renderer *terminalRenderer = new TerminalRenderer(cellSize, cols, rows);
+    // std::unique_ptr<Renderer> terminalRenderer = std::make_unique<TerminalRenderer>(cellSize, cols, rows);
     renderer->Setup("Conway's Game of Life");
 
     // Theoretically we could setup the grid to not be aware of the renderer, but for this example it felt fine to pass it a polymorphic renderer.
